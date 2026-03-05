69ea4babd02e55ff9d80a39adafd6e2a8a1c91da
EeshaniWeerasingha <dhanushikaeeshani@gmail.com>
2026-03-05 15:57:53 +0530

    Implement UnitHandler for metric and English units
    Added UnitHandler class to manage unit selection for height and weight.

--- BMI/src/Main.java
+++ BMI/src/Main.java
@@ -3,3 +3,28 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class UnitHandler {
+
+    public UnitHandler(JRadioButton metric, JRadioButton english,
+                       JLabel heightUnit, JLabel weightUnit){
+
+        metric.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                heightUnit.setText("meters");
+                weightUnit.setText("kg");
+            }
+        });
+
+        english.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                heightUnit.setText("inches");
+                weightUnit.setText("pounds");
+            }
+        });
+    }
+}
